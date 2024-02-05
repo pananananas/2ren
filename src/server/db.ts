fc3573e521e1db02d55ffa8f7143039666caaba6
Eryk Wójcik <101266606+pananananas@users.noreply.github.com>
2024-02-05 17:28:14 +0100

    fix to prisma implementation

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,21 +1,14 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env.js";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const client = new Client({ url: env.DATABASE_URL });
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const db =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(client),
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
