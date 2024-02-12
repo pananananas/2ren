0a768f64277047299041e72bf8b94cf6de32837b
Eryk Wójcik <101266606+pananananas@users.noreply.github.com>
2024-02-12 12:56:07 +0100

    added db content

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { withClerkMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export default withClerkMiddleware(() => {
-  console.log("middleware running");
+  // console.log("middleware running");
   return NextResponse.next();
 });
 
