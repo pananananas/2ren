e06c705753cfd9f9dcb4f8a92ab8a7ee97005e0e
Eryk Wójcik <101266606+pananananas@users.noreply.github.com>
2024-02-27 13:45:33 +0100

    optimized delete dialog for mobile

--- src/components/icons/icon-search.tsx
+++ src/components/icons/icon-search.tsx
@@ -11,9 +11,9 @@ export const IconSearch = (props: React.SVGProps<SVGSVGElement>) => {
         <path
           d="M15 15L10.9581 10.9581M10.9581 10.9581C12.0137 9.9025 12.6667 8.44416 12.6667 6.83333C12.6667 3.61167 10.055 1 6.83333 1C3.61167 1 1 3.61167 1 6.83333C1 10.055 3.61167 12.6667 6.83333 12.6667C8.44416 12.6667 9.9025 12.0137 10.9581 10.9581Z"
           stroke="#9DA3AE"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     );
